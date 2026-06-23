5feffac082b425b47c36b0cd073902171756d7a6
RicbaronR <ric.baron01@gmail.com>
2026-06-23 14:08:57 -0300

    feat: criação do endpoint para a listagem básica de compras

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -3,3 +3,4 @@ from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
 from .livro import LivroRetrieveSerializer, LivroSerializer, LivroListSerializer
 from .user import UserRegistrationSerializer, UserSerializer
+from .compra import CompraSerializer
