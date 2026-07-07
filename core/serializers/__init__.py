b2f7fff9595f04785ba067dfbd96aa260c8ee343
RicbaronR <ric.baron01@gmail.com>
2026-07-07 14:30:07 -0300

    feat: criação de um endpoint para criar novas compras

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -3,4 +3,9 @@ from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
 from .livro import LivroRetrieveSerializer, LivroSerializer, LivroListSerializer
 from .user import UserRegistrationSerializer, UserSerializer
-from .compra import CompraSerializer
+from .compra import (
+    CompraCreateUpdateSerializer,
+    CompraSerializer,
+    ItensCompraCreateUpdateSerializer,
+    ItensCompraSerializer,
+)
--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,7 +1,14 @@
 from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField
 
-from core.models import Compra, ItensCompra
+from core.models import Compra, ItensCompra, 
 
+
+class ItensCompraCreateUpdateSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ('livro', 'quantidade')
+
+        
 class ItensCompraSerializer(ModelSerializer):
     titulo = CharField(source='livro.titulo', read_only=True)
     editora = CharField(source='livro.editora.nome', read_only=True)
@@ -17,6 +24,23 @@ class ItensCompraSerializer(ModelSerializer):
         model = ItensCompra
         fields = ('titulo', 'editora', 'quantidade', 'preco', 'total', 'capa')
         depth = 1
+
+
+class CompraCreateUpdateSerializer(ModelSerializer):
+    itens = ItensCompraCreateUpdateSerializer(many=True)
+
+    class Meta:
+        model = Compra
+        fields = ('usuario', 'itens')
+
+    def create(self, validated_data):
+        itens_data = validated_data.pop('itens')
+        compra = Compra.objects.create(**validated_data)
+        for item_data in itens_data:
+            ItensCompra.objects.create(compra=compra, **item_data)
+        compra.save()
+        return compra
+
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
